942bd00d7ed7dca9e82590ac6c7a52262d98996c
RishabhWDB <rishabharya.k@gmail.com>
2026-02-27 13:32:44 +0530

    Breach Simulation

--- src/simulate_data.py
+++ src/simulate_data.py
@@ -21,6 +21,12 @@ def generate_shipment(shipment_id):
 
     baseline_temp = PRODUCT_BASELINES[product_type]["temp"]
     inside_temps = [baseline_temp+ np.random.normal(0,0.5) for _ in range(60)]
+    breach_probability = 0.4 if cooling_system == "standard" else 0.3
+    is_breached = random.random() < breach_probability
+    if is_breached:
+        failure_point = random.randint(20,45)
+        for i in range(failure_point, 60):
+            inside_temps[i] = inside_temps[i] + (i - failure_point) * 0.3 + np.random.normal(0,0.2)
     outside_temps = [np.random.uniform(25,40) for _ in range(60)]
     humidities = [np.random.uniform(60,90) for _ in range(60)]
     door_opens = [1 if np.random.random() < 0.05 else 0 for _ in range(60)]
@@ -33,7 +39,8 @@ def generate_shipment(shipment_id):
         "inside_temp" : inside_temps,
         "outside_temp" : outside_temps,
         "humidity" : humidities,
-        "door_opens" : door_opens
+        "door_opens" : door_opens,
+        "is_breached" : is_breached
 
     })
 
@@ -43,6 +50,8 @@ if __name__ == "__main__":
     df = generate_shipment(1)
     print(df.head(10))
     print(df.shape)
+    print(df["is_breached"].iloc[0])  # was it breached?
+    print(df["inside_temp"].tail(10))
 
 
 
