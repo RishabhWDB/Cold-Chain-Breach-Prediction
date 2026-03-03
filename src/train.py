a66fc2fd65fb7819531f5a8d11818175484b078a
RishabhWDB <rishabharya.k@gmail.com>
2026-03-03 16:32:37 +0530

    Restructured train.py

--- src/train.py
+++ src/train.py
@@ -17,22 +17,16 @@ def load_features():
     #print(y.shape)
 
     df = pd.get_dummies(df, columns = ["product_type", "cooling_system"])
-
     X = df.drop(columns = ["is_breached"])
 
     return X,y
 
 
-def train_model(X_train, y_train):
+def train_model(X_train, y_train, X_test, y_test, params):
     mlflow.set_experiment("cold-chain-breach-prediction")
 
     with mlflow.start_run(run_name="lightgbm"):
-        params = {
-        "n_estimators": 200,
-        "learning_rate": 0.05,
-        "max_depth": 6,
-        "random_state": 42
-        }
+
 
         model = LGBMClassifier(**params)
         model.fit(X_train, y_train)
@@ -43,7 +37,6 @@ def train_model(X_train, y_train):
         acc, f1, report = evaluate_model(model, X_test, y_test)
         mlflow.log_metric("accuracy", acc)
         mlflow.log_metric("f1", f1)
-
         mlflow.lightgbm.log_model(model, "model")
 
         return model
@@ -66,7 +59,15 @@ if __name__ == "__main__":
     X_train, X_test, y_train, y_test = train_test_split(X, y , test_size = 0.2, random_state = 42)
     #print(y_train.dtypes)
     #print(type(y_train))
-    model = train_model(X_train, y_train)
+
+    params = {
+        "n_estimators": 200,
+        "learning_rate": 0.05,
+        "max_depth": 6,
+        "random_state": 42
+        }
+    
+    model = train_model(X_train, y_train, X_test, y_test, params)
 
     acc, f1, report = evaluate_model(model, X_test, y_test)
 
